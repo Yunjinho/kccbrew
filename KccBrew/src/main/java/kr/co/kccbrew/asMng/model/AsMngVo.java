a4819c11e9663ab8fa11cb3546413f8c0cdf131a
Yunjinho <dbswlsgh1238@naver.com>
2023-09-06 15:36:04 +0900

    feat: login jsp 파일 추가

--- KccBrew/src/main/java/kr/co/kccbrew/asMng/model/AsMngVo.java
+++ KccBrew/src/main/java/kr/co/kccbrew/asMng/model/AsMngVo.java
@@ -4,33 +4,31 @@ import java.sql.Date;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 
 /**
  * AS 관련 vo
  */
 @AllArgsConstructor
-@NoArgsConstructor
 @Data
 public class AsMngVo {
 	//List 조회
-	int asInfoSeq;
-	Date startDate;
-	Date endDate;
-	Date regDttm;
-	String userId;
-	String storeAddr;
-	String machineCd;
-	String statusCd;
-	String storeNm;
-	String locationCd;
+	private String asInfoSeq;
+	private String startYr;
+	private String startMn;
+	private String endYr;
+	private String endMn;
+	private Date regDttm;
+	private String userId;
+	private String storeAddr;
+	private String machineCd;
+	private String statusCd;
+	private String storeNm;
+	private String locationCd;
 	
-	
-	Date modDttm;
-	int fileSeq;
-	String asContent;
-	
+	private Date modDttm;
+	private int fileSeq;
+	private String asContent;
 	
 	//장비 코드 리스트
 	//as 상태 코드 리스트
@@ -39,5 +37,20 @@ public class AsMngVo {
 	private String grpCdId;
 	private String grpCdNm;
 	private String grpCdDtlNm;
+	
+	//인자 없는 생성자
+	public AsMngVo() {
+		this.asInfoSeq="";
+		this.startYr="";
+		this.startMn="";
+		this.endYr="";
+		this.endMn="";
+		this.userId="";
+		this.storeAddr="";
+		this.machineCd="";
+		this.statusCd="";
+		this.storeNm="";
+	}
+
 	
 }
--- KccBrew/src/main/java/kr/co/kccbrew/asMng/service/IAsMngService.java
+++ KccBrew/src/main/java/kr/co/kccbrew/asMng/service/IAsMngService.java
@@ -2,6 +2,8 @@ package kr.co.kccbrew.asMng.service;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import kr.co.kccbrew.asMng.model.AsMngVo;
 
 public interface IAsMngService {
@@ -9,7 +11,12 @@ public interface IAsMngService {
 	 * @param asMngVo : 검색 조건 들어있는 Vo
 	 * @return : 조건으로 검색한 as 리스트
 	 */
-	public List<AsMngVo>selectASList(AsMngVo asMngVo);
+	public List<AsMngVo>selectASList(AsMngVo asMngVo,int page);
+	/**
+	 * @return AS 리스트의 총 수
+	 */
+	public int countASList(AsMngVo asMngVo);
+	
 	/**
 	 * 장비 코드 리스트 조회
 	 * @return : 장비 코드 리스트
