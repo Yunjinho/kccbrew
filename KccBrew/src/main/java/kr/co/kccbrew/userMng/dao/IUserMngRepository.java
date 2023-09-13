8ef8ccc1f6b4220a86717b537271241b131988e8
qocn0430 <zzzz_-z@nate.com>
2023-09-13 09:41:20 +0900

    feat 회원 조회,상세 (배수연2023-09-13)

---
+++ KccBrew/src/main/java/kr/co/kccbrew/userMng/dao/IUserMngRepository.java
@@ -0,0 +1,19 @@
+package kr.co.kccbrew.userMng.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Param;
+
+import kr.co.kccbrew.userMng.model.UserMngVo;
+
+public interface IUserMngRepository {
+	List<UserMngVo> userList(Map<String, Object> map);
+	int getUserCount(Map<String, Object> map);
+	List<UserMngVo> newList();
+	 UserMngVo findByUserId(String userId);
+	 UserMngVo findByUserInfo(@Param("userId")String userId, @Param("userTypeCd") UserMngVo userTypeCd);
+	 void save(UserMngVo user);
+	 int getNewCount();
+
+}
