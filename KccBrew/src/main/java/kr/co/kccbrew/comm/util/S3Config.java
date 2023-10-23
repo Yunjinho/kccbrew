635faccf93223533a30d92a0f6b514b2cf593dfd
Yunjinho <dbswlsgh1238@naver.com>
2023-10-23 16:28:03 +0900

    aws key 값 숨김

--- KccBrew/src/main/java/kr/co/kccbrew/comm/util/S3Config.java
+++ KccBrew/src/main/java/kr/co/kccbrew/comm/util/S3Config.java
@@ -11,33 +11,24 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
 @Configuration
 public class S3Config {
-	
-    //@Value("${cloud.aws.credentials.accessKey}")
-    private String accessKey="AKIA5PQESN3XLEESCDFT";
-    //@Value("${cloud.aws.credentials.secretKey}")
-    private String secretKey="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";
-    //@Value("${cloud.aws.s3.bucketName}")
-    private String bucketName="kccbrewbucket";
-   // @Value("${cloud.aws.region.static}")
-    private String region="ap-northeast-2";
-
-/*    @Value("${cloud.aws.credentials.accessKey}")
-    private String accessKey;
+    
+    @Value("${cloud.aws.credentials.accessKey}")
+    public String accessKey;
     @Value("${cloud.aws.credentials.secretKey}")
-    private String secretKey;
+    public String secretKey;
     @Value("${cloud.aws.s3.bucketName}")
-    private String bucketName;
+    public String bucketName;
     @Value("${cloud.aws.region.static}")
-    private String region;*/
-
-    @Autowired
+    public String region;
+    
+    
+    @Bean
     public AmazonS3 amazonS3() {
-    	System.out.println(accessKey);
-    	System.out.println(secretKey);
-    	System.out.println(bucketName);
-    	System.out.println(region);
         AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
         return AmazonS3ClientBuilder
                 .standard()
@@ -45,6 +36,4 @@ public class S3Config {
                 .withRegion(region)
                 .build();
     }
-    
-    
 }
