4e7e4930d367939d0085bc3b97dd0ff366ebbdcc
seeun <seeun0426@gmail.com>
2023-10-28 16:10:33 +0900

    feat : as상태가 01(접수중)인 경우의 로직 구현중2 / 2023-10-28 이세은

--- KccBrew/src/main/java/kr/co/kccbrew/batch/MybatisItemProcessor.java
+++ KccBrew/src/main/java/kr/co/kccbrew/batch/MybatisItemProcessor.java
@@ -3,17 +3,25 @@ package kr.co.kccbrew.batch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import kr.co.kccbrew.asMng.model.AsMngVo;
+import kr.co.kccbrew.asMng.service.IAsMngService;
 
 @Component("mybatisItemProcessor")
 public class MybatisItemProcessor implements ItemProcessor<AsLogVo, AsMngVo>{
     private static Logger logger = LoggerFactory.getLogger(MybatisItemProcessor.class);
     
+    @Autowired
+    private IAsMngService asMngService;
+    
     @Override
     public AsMngVo process(AsLogVo asLogVo) throws Exception {
     	String asStatus = asLogVo.getAsStatus();
+    	if (asStatus.equals("1")) {
+    		
+    	}
     	
 
         logger.info("변경 대상 가수 정보: " +   ", 변경 후: " );
